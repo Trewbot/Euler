3ed81191feac1700bdaf2b317c9eecf3926f115b
Trevor Hoglund <trewbot@phene.co>
2015-10-20 11:56:48 -0500

    Renamed TOP to top

--- 002.py
+++ 002.py
@@ -1,9 +1,9 @@
 import sys
-TOP = (len(sys.argv) > 1 and int(sys.argv[1])) or 4e6
+top = (len(sys.argv) > 1 and int(sys.argv[1])) or 4e6
 m = 1
 n = 2
 sum = 0
-while n <= TOP:
+while n <= top:
 	if n % 2 == 0:
 		sum += n
 	o = n

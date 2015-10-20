a6f4101928f2fd8326f227972cf62cc9b16784ed
Trevor Hoglund <trewbot@phene.co>
2015-10-20 12:41:10 -0500

    Fixed 004 returning incorrect answers

--- 004.py
+++ 004.py
@@ -8,15 +8,15 @@ def largestPalindrome(dig):
 	min = 10 ** (dig - 1)
 	max = (10 ** (dig)) - 1
 	a = max
-	top = 0
+	top = (10 ** (dig)) + 1
 	while a >= min:
 		b = max
 		while b >= a:
 			p = a * b
 			if p > top and palindrome(p):
-				return p
+				top = p
 			b -= 1
 		a -= 1
-	
+	return top
 dig = (len(sys.argv) > 1 and int(sys.argv[1])) or 3
 print(largestPalindrome(dig))

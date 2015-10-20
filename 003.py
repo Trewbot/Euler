ba0598977e132da28d184938ff5dd18878af3c12
Trevor Hoglund <trewbot@phene.co>
2015-10-20 11:56:14 -0500

    Added 003

---
+++ 003.py
@@ -0,0 +1,15 @@
+import sys
+num = (len(sys.argv) > 1 and int(sys.argv[1])) or 600851475143
+def factors(n):
+	factors = []
+	d = 2
+	while n > 1:
+		while n % d == 0:
+			factors.append(int(d))
+			n /= d
+		d += 1
+		if d**2 > n:
+			if n > 1: factors.append(int(n))
+			break
+	return factors
+print(max(factors(num)))

fd059ebdb72cd56e541bfc2031d1030e57dfb3f7
Trevor Hoglund <trewbot@phene.co>
2015-10-21 13:34:44 -0500

    Added 009

---
+++ 009.py
@@ -0,0 +1,28 @@
+import sys
+import time
+import math
+
+def isPerfectSquare(n):
+	return math.sqrt(n) % 1 == 0
+
+def findPythagoreanTripleProduct(sum):
+	a = 1
+	while a < sum:
+		b = 1
+		while b <= a:
+			if not isPerfectSquare((a ** 2) + (b ** 2)):
+				b += 1
+				continue
+			c = int(math.sqrt((a ** 2) + (b ** 2)))
+			if a + b + c == 1e3:
+				return a * b * c
+			b += 1
+		a += 1
+
+sum = (len(sys.argv) > 1 and int(sys.argv[1])) or 1000
+
+print()
+start = time.time()
+print(findPythagoreanTripleProduct(sum))
+print("%.3f seconds" % (time.time() - start))
+print()
